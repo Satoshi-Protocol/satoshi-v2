487f55ab454b506742e3edb20ca1b7a0a1c27a58
LI-YONG-QI <a2369028@gmail.com>
2024-10-24 17:42:07 +0800

    deploy: debt token

--- deploy/DebtToken.ts
+++ deploy/DebtToken.ts
@@ -1,20 +1,48 @@
 import assert from 'assert'
 
 import { type DeployFunction } from 'hardhat-deploy/types'
-import { upgrades, ethers } from "hardhat";
+
+const fs = require('fs')
+const path = require('path')
+
+interface Transaction {
+    contractName: string
+    transactionType: string
+    contractAddress: string
+}
 
 const contractName = 'DebtToken'
+const NAME = 'satUSD.t'
+const SYMBOL = 'satUSD.t'
+
+// get the address of the SatoshiXApp contract from broadcast(forge) recordings
+const getSatoshiXAppAddress = (chainId: number, timestamp?: number) => {
+    const time = timestamp ? `${timestamp}` : 'latest'
+
+    const coreDevelopment = JSON.parse(
+        fs.readFileSync(path.resolve(__dirname, `../broadcast/Deploy.s.sol/${chainId}/run-${time}.json`), 'utf8')
+    )
+
+    const txs = coreDevelopment.transactions
+    const satoshiXAppTx: Transaction[] = txs.filter(
+        (tx: Transaction) => tx.contractName === 'SatoshiXApp' && tx.transactionType == 'CREATE'
+    )
+
+    return satoshiXAppTx[0].contractAddress
+}
 
 const deploy: DeployFunction = async (hre) => {
     const { getNamedAccounts, deployments } = hre
 
-    const { deploy } = deployments
+    //const { deploy } = deployments
     const { deployer } = await getNamedAccounts()
 
     assert(deployer, 'Missing named deployer account')
 
     console.log(`Network: ${hre.network.name}`)
     console.log(`Deployer: ${deployer}`)
+
+    const satoshiXAppAddr = getSatoshiXAppAddress(hre.network.config.chainId!)
 
     // This is an external deployment pulled in from @layerzerolabs/lz-evm-sdk-v2
     //
@@ -32,12 +60,14 @@ const deploy: DeployFunction = async (hre) => {
     //     eid: EndpointId.AVALANCHE_V2_TESTNET
     //   }
     // }
-    const endpointV2Deployment = await hre.deployments.get('EndpointV2')
-    const DebtTokenFactory = await ethers.getContractFactory(contractName);
 
-    const debtTokenProxy = await upgrades.deployProxy(DebtTokenFactory, ["satUSD.t","satUSD.t","0x0000000000000000000000000000000000000000", deployer], { initializer: "initialize" });
+    const endpointV2Deployment = await deployments.get('EndpointV2')
+    const DebtTokenFactory = await hre.ethers.getContractFactory(contractName)
 
-
+    const debtTokenProxy = await hre.upgrades.deployProxy(DebtTokenFactory, [NAME, SYMBOL, satoshiXAppAddr, deployer], {
+        initializer: 'initialize',
+        constructorArgs: [endpointV2Deployment.address],
+    })
 
     console.log(`Deployed contract: ${contractName}, network: ${hre.network.name}, address: ${debtTokenProxy.address}`)
 }
--- script/DeployDebtToken.s.sol
+++ script/DeployDebtToken.s.sol
@@ -1,18 +1,28 @@
+//! LEGACY CODE
+
 // // SPDX-License-Identifier: MIT
 // pragma solidity ^0.8.20;
 
 // import {Script, console} from "forge-std/Script.sol";
 // import {IDebtToken} from "../src/core/interfaces/IDebtToken.sol";
-// import {DeployBase} from "./utils/DeployBase.sol";
+// import {Deployer} from "./utils/Deployer.sol";
 
-// contract DeployDebtToken is Script, DeployBase {
+// contract DeployDebtToken is Script {
 //     uint256 public constant MOCK_PK = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
+//     address public constant LZ_ENDPOINT = 0x1234567890123456789012345678901234567890;
 //     address public satoshiXApp = 0x1234567890123456789012345678901234567890;
 
+//     IDebtToken debtToken;
+
 //     function run() public {
+//         _deployDebtToken();
+//     }
+
+//     function _deployDebtToken() internal view returns (address) {
 //         vm.startBroadcast(MOCK_PK);
 
-//         _deployDebtToken();
+//         debtToken = Deployer._deployDebtToken(satoshiXApp, LZ_ENDPOINT);
 
 //         vm.stopBroadcast();
 //     }
