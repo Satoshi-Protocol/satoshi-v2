eb9e8f992fe038ac4cb66f0a237eb1a1625d3146
ashirleyshe <ashirleyshe@gmail.com>
2025-06-19 17:43:21 +0800

    feat(periphery): add swapIn

--- src/core/helpers/SatoshiPeriphery.sol
+++ src/core/helpers/SatoshiPeriphery.sol
@@ -9,6 +9,7 @@ import { DebtTokenWithLz } from "../DebtTokenWithLz.sol";
 import { IBorrowerOperationsFacet } from "../interfaces/IBorrowerOperationsFacet.sol";
 import { ICoreFacet } from "../interfaces/ICoreFacet.sol";
 import { ILiquidationFacet } from "../interfaces/ILiquidationFacet.sol";
+import { INexusYieldManagerFacet } from "../interfaces/INexusYieldManagerFacet.sol";
 import { ITroveManager } from "../interfaces/ITroveManager.sol";
 
 import { IDebtToken } from "../interfaces/IDebtToken.sol";
@@ -279,6 +280,14 @@ contract SatoshiPeriphery is ISatoshiPeriphery, UUPSUpgradeable, OwnableUpgradea
 
         _afterWithdrawDebt(userDebtAmount, _lzSendParam);
         _afterWithdrawColl(troveManager.collateralToken(), userCollAmount);
+    }
+
+    function swapIn(address asset, uint256 assetAmount, LzSendParam calldata _lzSendParam) external payable {
+        _beforeAddColl(IERC20(asset), assetAmount);
+
+        uint256 debtAmount = INexusYieldManagerFacet(xApp).swapIn(asset, address(this), assetAmount);
+
+        _afterWithdrawDebt(debtAmount, _lzSendParam);
     }
 
     // INTERNAL FUNCTIONS //
